e338b9b9321a1f291d0249485f8e0ce451c5abe1
Harrison Harnisch <hharnisc@gmail.com>
2017-03-20 22:33:12 -0500

    Use NavBar in app

---
+++ packages/web/components/App/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { NavBar } from '@bufferapp/components';
+
+const App = () => <NavBar title={'Buffer'} />;
+
+export default App;
--- packages/web/components/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Text } from '@bufferapp/components';
-
-const App = () => <Text>hi 👋</Text>;
-
-export default App;
