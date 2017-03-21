88e6a65c70e34cfa63a5cfdedc328d3ab6b08433
Harrison Harnisch <hharnisc@gmail.com>
2017-03-21 18:25:34 -0500

    Stub in update data

--- packages/update-queue/components/index.jsx
+++ packages/update-queue/components/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
-const UpdateQueue = () => <div>UpdateQueue</div>;
+const UpdateQueue = ({ updates }) =>
+  <div>{JSON.stringify(updates, null, 2)}</div>;
 
 export default UpdateQueue;
