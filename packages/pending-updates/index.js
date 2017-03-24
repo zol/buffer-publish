a7828b48114a94e3eb203ec6f0eab35a208b5e8d
Harrison Harnisch <hharnisc@gmail.com>
2017-03-23 21:51:48 -0500

    Make sure container and component are rendered

--- packages/pending-updates/index.js
+++ packages/pending-updates/index.js
@@ -1,1 +1,8 @@
+import { connect } from 'react-redux';
+import PendingUpdates from './components';
+
+export default connect(
+  () => ({}),
+)(PendingUpdates);
+
 export { reducer, actions, actionTypes } from './reducer';
---
+++ packages/pending-updates/index.test.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { mount } from 'enzyme';
+import PendingUpdatesContainer, { reducer, actions, actionTypes } from './index';
+import PendingUpdates from './components';
+
+const storeFake = state => ({
+  default: () => {},
+  subscribe: () => {},
+  dispatch: () => {},
+  getState: () => ({ ...state }),
+});
+
+describe('pending-updates', () => {
+  it('should export reducer, actions and actionTypes', () => {
+    expect(reducer)
+      .toBeDefined();
+    expect(actions)
+      .toBeDefined();
+    expect(actionTypes)
+      .toBeDefined();
+  });
+
+  describe('PendingUpdatesContainer', () => {
+    it('should render a container', () => {
+      const store = storeFake({});
+      const wrapper = mount(
+        <Provider store={store}>
+          <PendingUpdatesContainer />
+        </Provider>,
+      );
+      expect(wrapper.find(PendingUpdatesContainer).length)
+        .toBe(1);
+      expect(wrapper.find(PendingUpdates).length)
+        .toBe(1);
+    });
+  });
+});
