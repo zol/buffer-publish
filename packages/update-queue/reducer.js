18648b07f9df97a1a30c8777086f680409c217e3
Harrison Harnisch <hharnisc@gmail.com>
2017-03-23 20:52:37 -0500

    Implement SET_PENDING_UPDATES reducer

--- packages/update-queue/reducer.js
+++ packages/update-queue/reducer.js
@@ -1,3 +1,12 @@
-const reducer = (state = []) => state;
+export const SET_PENDING_UPDATES = 'SET_PENDING_UPDATES';
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case SET_PENDING_UPDATES:
+      return action.pendingUpdates;
+    default:
+      return state;
+  }
+};
 
 export default reducer;
--- packages/update-queue/reducer.test.js
+++ packages/update-queue/reducer.test.js
@@ -1,8 +1,24 @@
-import reducer from './reducer';
+import deepFreeze from 'deep-freeze';
+import reducer, { SET_PENDING_UPDATES } from './reducer';
+import pendingUpdates from './examplePendingUpdates';
 
 describe('reducer', () => {
   it('should return initial state', () => {
-    expect(reducer(undefined, {}))
-      .toEqual([]);
+    const stateBefore = undefined;
+    const stateAfter = [];
+    expect(reducer(stateBefore, {}))
+      .toEqual(stateAfter);
+  });
+  it('should set updates from initial state', () => {
+    const stateBefore = undefined;
+    const action = {
+      type: SET_PENDING_UPDATES,
+      pendingUpdates,
+    };
+    const stateAfter = pendingUpdates;
+    deepFreeze(action);
+    deepFreeze(stateAfter);
+    expect(reducer(stateBefore, action))
+      .toEqual(stateAfter);
   });
 });
