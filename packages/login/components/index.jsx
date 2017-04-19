30c1eef458eac3d96690050bafdcafe40415784a
Harrison Harnisch <hharnisc@gmail.com>
2017-04-19 17:49:02 -0500

    basic login layout

--- packages/login/components/index.jsx
+++ packages/login/components/index.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import {
+  Button,
+  InputEmail,
+  InputPassword,
+} from '@bufferapp/components';
 
 const LoginForm = () =>
-  <div>LoginForm</div>;
+  <form>
+    <div>
+      <Field
+        name={'email'}
+        component={InputEmail}
+        label={'Email'}
+        placeholder={'hello@buffer.com'}
+      />
+    </div>
+    <div>
+      <Field
+        name={'password'}
+        component={InputPassword}
+        label={'Password'}
+      />
+    </div>
+    <div>
+      <Button>Login</Button>
+    </div>
+  </form>;
 
-export default LoginForm;
+export default reduxForm({
+  form: 'login',
+})(LoginForm);
--- packages/login/components/story.jsx
+++ packages/login/components/story.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer as formReducer } from 'redux-form';
 import LoginForm from './index';
 
+const store = createStore(combineReducers({ form: formReducer }));
+
+console.log('store', store);
+
 storiesOf('LoginForm')
+  .addDecorator(getStory =>
+    <Provider store={store}>
+      {getStory()}
+    </Provider>,
+  )
   .add('LoginForm', () => (
     <LoginForm />
   ));
