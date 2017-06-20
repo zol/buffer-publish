590a877906731f705a857863cd77ab2aa27f3613
Harrison Harnisch <hharnisc@gmail.com>
2017-06-20 14:27:54 -0700

    Stub in login package

---
+++ packages/login/reducer.js
@@ -0,0 +1,19 @@
+export const actionTypes = {
+  LOGIN_START: 'LOGIN_START',
+  LOGIN_FAIL: 'LOGIN_FAIL',
+  LOGIN_SUCCESS: 'LOGIN_SUCCESS',
+};
+
+export const actions = {
+  loginStart: () => ({
+    type: actionTypes.LOGIN_START,
+  }),
+  loginSuccess: ({ sessionToken }) => ({
+    type: actionTypes.LOGIN_SUCCESS,
+    sessionToken,
+  }),
+  loginFail: ({ errorMessage }) => ({
+    type: actionTypes.LOGIN_FAIL,
+    errorMessage,
+  }),
+};
---
+++ packages/login/reducer.test.js
@@ -0,0 +1,39 @@
+import deepFreeze from 'deep-freeze';
+import {
+  actions,
+  actionTypes,
+} from './reducer';
+
+
+describe('actions', () => {
+  it('should create loginStart action', () => {
+    const actionAfter = {
+      type: actionTypes.LOGIN_START,
+    };
+    deepFreeze(actionAfter);
+    expect(actions.loginStart())
+      .toEqual(actionAfter);
+  });
+
+  it('should create loginFail action', () => {
+    const errorMessage = 'something bad happend';
+    const actionAfter = {
+      type: actionTypes.LOGIN_FAIL,
+      errorMessage,
+    };
+    deepFreeze(actionAfter);
+    expect(actions.loginFail({ errorMessage }))
+      .toEqual(actionAfter);
+  });
+
+  it('should create loginSuccess action', () => {
+    const sessionToken = 'some session token';
+    const actionAfter = {
+      type: actionTypes.LOGIN_SUCCESS,
+      sessionToken,
+    };
+    deepFreeze(actionAfter);
+    expect(actions.loginSuccess({ sessionToken }))
+      .toEqual(actionAfter);
+  });
+});
