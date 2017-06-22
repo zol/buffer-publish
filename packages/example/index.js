b8e58f6364db05b42527e25d728f78a2eb071334
Harrison Harnisch <hharnisc@gmail.com>
2017-06-22 09:04:15 -0700

    add example component into we app

--- packages/example/index.js
+++ packages/example/index.js
@@ -1,1 +1,10 @@
-export reducer from './reducer';
+import { connect } from 'react-redux';
+import LoggedIn from './components/LoggedIn';
+
+export default connect(
+  state => ({
+    loggedIn: state.example.loggedIn,
+  }),
+)(LoggedIn);
+
+export reducer, { actions, actionTypes } from './reducer';
--- packages/web/components/App/index.jsx
+++ packages/web/components/App/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { NavBar } from '@bufferapp/components';
+import LoggedIn from '@bufferapp/example';
 
 const App = () =>
   <div>
     <NavBar title={'Buffer'} />
+    <LoggedIn />
   </div>;
 
 export default App;
