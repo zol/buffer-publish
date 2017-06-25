233db5e37221dc42f17b7e6907b552b2e9762478
Harrison Harnisch <hharnisc@gmail.com>
2017-06-25 12:45:00 -0500

    fix tests and jest configuration

--- packages/web/components/App/story.jsx
+++ packages/web/components/App/story.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import createStore from '@bufferapp/store';
+import { Provider } from 'react-redux';
+import { checkA11y } from 'storybook-addon-a11y';
 import App from './index';
 
 storiesOf('App')
+  .addDecorator(checkA11y)
+  .addDecorator(getStory =>
+    <Provider store={createStore()}>
+      {getStory()}
+    </Provider>,
+  )
   .add('should render application', () => (
     <App />
   ));
