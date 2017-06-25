1ec0a8963c5342760c8419039f1c9569dddd8d10
Harrison Harnisch <hharnisc@gmail.com>
2017-06-25 12:05:38 -0500

    fix example package tests

--- packages/example/reducer.test.js
+++ packages/example/reducer.test.js
@@ -1,4 +1,5 @@
 import deepFreeze from 'deep-freeze';
+import { actionTypes as loginActionTypes } from '@bufferapp/login';
 import reducer from './reducer';
 
 describe('reducer', () => {
@@ -11,6 +12,50 @@ describe('reducer', () => {
     };
     deepFreeze(action);
     expect(reducer(undefined, action))
+      .toEqual(stateAfter);
+  });
+
+  it('should handle LOGIN_SUCCESS action type', () => {
+    const stateAfter = {
+      loggedIn: true,
+    };
+    const action = {
+      type: loginActionTypes.LOGIN_SUCCESS,
+    };
+    deepFreeze(action);
+    expect(reducer(undefined, action))
+      .toEqual(stateAfter);
+  });
+
+  it('should handle LOGOUT actionType', () => {
+    const stateBefore = {
+      loggedIn: true,
+    };
+    const stateAfter = {
+      loggedIn: false,
+    };
+    const action = {
+      type: loginActionTypes.LOGOUT,
+    };
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+    expect(reducer(stateBefore, action))
+      .toEqual(stateAfter);
+  });
+
+  it('should handle LOGIN_FAIL actionType', () => {
+    const stateBefore = {
+      loggedIn: true,
+    };
+    const stateAfter = {
+      loggedIn: false,
+    };
+    const action = {
+      type: loginActionTypes.LOGIN_FAIL,
+    };
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+    expect(reducer(stateBefore, action))
       .toEqual(stateAfter);
   });
 });
