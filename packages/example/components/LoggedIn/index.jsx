7f17ad2d93f2913336602cefb4cd97a25b60dbda
Harrison Harnisch <hharnisc@gmail.com>
2017-06-29 12:45:58 -0500

    use Buffer Component in example package

--- packages/example/components/LoggedIn/index.jsx
+++ packages/example/components/LoggedIn/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  Text,
+} from '@bufferapp/components';
 
 const LoggedIn = ({
   loggedIn,
   translations,
 }) =>
-  <div>{ loggedIn ? translations.loggedIn : translations.loggedOut }</div>;
+  <Text>{ loggedIn ? translations.loggedIn : translations.loggedOut }</Text>;
 
 LoggedIn.propTypes = {
   loggedIn: PropTypes.bool,
