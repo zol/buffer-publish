fa6e011bb86b2f0e9e74a59872f2fe2213c3182f
Dan Farrelly <daniel.j.farrelly@gmail.com>
2017-06-30 18:39:06 -0400

    Add pre-build script for the deployment pipeline

---
+++ pre-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+npm run init
+npm run build
+
+UPLOADER="https://github.com/bufferapp/buffer-static-upload/releases/download/0.1.0/buffer-static-upload-`uname -s`"
+curl -L $UPLOADER > ./buffer-static-upload
+chmod +x ./buffer-static-upload
+
+FILES="bundle.js"
+./buffer-static-upload -files "$FILES" -dir publish
