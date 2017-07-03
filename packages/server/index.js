b43944f47a8c44a417be31c18b0137d791294cc7
Dan Farrelly <daniel.j.farrelly@gmail.com>
2017-07-03 14:29:48 -0400

    Disable eslint rule for local dev block

--- packages/server/index.js
+++ packages/server/index.js
@@ -4,10 +4,12 @@ const service = require('./service');
 const middleware = [];
 
 if (process.env.NODE_ENV === 'development') {
+  /* eslint-disable global-require */
   const webpack = require('webpack');
   const config = require('./webpack.config.dev');
   const webpackMiddleware = require('webpack-dev-middleware');
   const webpackHotMiddleware = require('webpack-hot-middleware');
+  /* eslint-disable */
 
   const compiler = webpack(config);
   const wpMiddleware = (next) => {
