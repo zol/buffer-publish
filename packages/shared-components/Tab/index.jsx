1dcbb41c7911e5f2898098949f700b4972eda246
Emily <eplummer26@gmail.com>
2017-07-05 08:47:18 -0700

    use calculateStyles

--- packages/shared-components/Tab/index.jsx
+++ packages/shared-components/Tab/index.jsx
@@ -6,35 +6,44 @@ import {
 import {
   Button,
 } from '@bufferapp/components';
+import {
+  calculateStyles,
+} from '@bufferapp/components/lib/utils';
 import HoverableText from '../HoverableText';
-
-const tabStyle = (active) => { //eslint-disable-line
-  return {
-    borderBottom: active ? `2px solid ${curiousBlue}` : '',
-    padding: '0 4px 16px 4px',
-    margin: '0 8px 0 8px',
-    display: 'inline-block',
-    minWidth: '60px',
-    textAlign: 'center',
-  };
-};
 
 const Tab = ({
   children,
   active,
   onClick,
-}) =>
-  <div
-    style={tabStyle(active)}
-  >
-    <Button noStyle onClick={onClick}>
-      <HoverableText
-        hoverColor={'black'}
-      >
-        {children}
-      </HoverableText>
-    </Button>
-  </div>;
+}) => {
+  const style = calculateStyles({
+    default: {
+      padding: '0 4px 16px 4px',
+      margin: '0 8px 0 8px',
+      display: 'inline-block',
+      minWidth: '60px',
+      textAlign: 'center',
+    },
+    active: {
+      borderBottom: active ? `2px solid ${curiousBlue}` : '',
+    },
+  }, {
+    active,
+  });
+  return (
+    <div
+      style={style}
+    >
+      <Button noStyle onClick={onClick}>
+        <HoverableText
+          hoverColor={'black'}
+        >
+          {children}
+        </HoverableText>
+      </Button>
+    </div>
+  );
+};
 
 Tab.propTypes = {
   active: PropTypes.bool.isRequired,
