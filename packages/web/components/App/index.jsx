5d37fb34df2afabaad45b49cac759015416ca94d
Emily <eplummer26@gmail.com>
2017-07-10 15:36:54 -0700

    adding dependencies + tweaks to web

--- packages/web/components/App/index.jsx
+++ packages/web/components/App/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { NavBar } from '@bufferapp/components';
 import LoggedIn from '@bufferapp/example';
+import { TabNavigation } from '@bufferapp/tabs';
 
+const onClick = () => console.log('hi');
 const App = () =>
   <div>
     <NavBar title={'Buffer'} />
     <LoggedIn />
-    <h1>Welcome to Buffer Publish!</h1>
+    <TabNavigation
+      onTabClick={onClick}
+      activeTabId={'12345'}
+    />
+    <h1>Welcome to Buffer Publishh!</h1>
   </div>;
 
 export default App;
