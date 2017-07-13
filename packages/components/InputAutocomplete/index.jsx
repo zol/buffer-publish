2c64f9d0268b5946af7cb02d124896d0ff9517b7
Emily <eplummer26@gmail.com>
2017-07-13 11:19:53 -0700

    add autocomplete atom

---
+++ packages/components/InputAutocomplete/index.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import uuid from 'uuid';
+import Autocomplete from 'react-autocomplete';
+import PropTypes from 'prop-types';
+import Text from '../Text';
+import {
+  calculateStyles,
+} from '../lib/utils';
+import {
+  aquaHaze,
+} from '../style/color';
+
+const matchValueToItem = (item, value) =>
+  item.toLowerCase().indexOf(value.toLowerCase()) !== -1 ||
+  item.toLowerCase().indexOf(value.toLowerCase()) !== -1;
+
+const InputAutocomplete = ({
+  onChange,
+  onSelect,
+  value,
+  items,
+  sortItems,
+}) => {
+  const renderItem = (item, isHighlighted) => {
+    const style = calculateStyles({
+      default: {
+        padding: '2px 6px',
+        cursor: 'default',
+        display: 'block',
+        position: 'relative',
+      },
+      isHighlighted: {
+        color: 'white',
+        background: `${aquaHaze}`,
+        padding: '2px 6px',
+        cursor: 'default',
+      },
+    }, {
+      isHighlighted,
+    });
+    return (
+      <div style={style}><Text
+        key={uuid()}
+      >
+        {item}
+      </Text></div>
+    );
+  };
+
+  const inputStyle = {
+    width: '318px',
+    margin: '0 0 0 .5rem',
+    padding: '.5rem',
+    fontSize: '.8rem',
+    backgroundColor: '#fff',
+    border: '1px solid #e6ebef',
+    borderRadius: '2px',
+  };
+
+  const menuStyle = {
+    margin: '0.25rem 0 0 0',
+    backgroundColor: 'white',
+    border: '1px solid #e6ebef',
+    borderRadius: '2px',
+    boxShadow: '0 1px 2px rgba(50, 59, 67, 0.3)',
+  };
+  return (
+    <div>
+      <Autocomplete
+        value={value}
+        inputProps={{ id: 'states-autocomplete', style: inputStyle }}
+        items={items}
+        getItemValue={(item) => item}
+        shouldItemRender={matchValueToItem}
+        sortItems={sortItems}
+        onChange={onChange}
+        onSelect={onSelect}
+        renderItem={renderItem}
+        menuStyle={menuStyle}
+      />
+    </div>
+  );
+};
+
+InputAutocomplete.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.string),
+  onChange: PropTypes.func.isRequired,
+  onSelect: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  sortItems: PropTypes.func.isRequired,
+};
+
+InputAutocomplete.defaultProps = {
+  value: '',
+  items: [],
+};
+
+export default InputAutocomplete;
