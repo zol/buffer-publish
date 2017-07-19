47903823e72d0d69957962a0b1d4bf0dd3238954
Melissa Alvarez <melissa@buffer.com>
2017-07-19 16:58:19 -0600

    Add composer placeholder to queue (#52)
    * Add composer placeholder to queue
    
    * update queue test snapshot with new composer input

--- packages/queue/components/QueuedPosts/index.jsx
+++ packages/queue/components/QueuedPosts/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
+  Input,
+} from '@bufferapp/components';
+import {
   PostList,
 } from '@bufferapp/publish-shared-components';
 
-// TODO: Add composer placeholder
+const composerStyle = {
+  marginBottom: '1.5rem',
+};
+
 const QueuedPosts = ({
   listHeader,
   loading,
@@ -19,15 +25,22 @@ const QueuedPosts = ({
     return (<div>Loading...</div>);
   }
   return (
-    <PostList
-      listHeader={listHeader}
-      posts={posts}
-      onCancelConfirmClick={onCancelConfirmClick}
-      onDeleteClick={onDeleteClick}
-      onDeleteConfirmClick={onDeleteConfirmClick}
-      onEditClick={onEditClick}
-      onShareNowClick={onShareNowClick}
-    />
+    <div>
+      <div style={composerStyle}>
+        <Input
+          placeholder={'What would you like to share?'}
+        />
+      </div>
+      <PostList
+        listHeader={listHeader}
+        posts={posts}
+        onCancelConfirmClick={onCancelConfirmClick}
+        onDeleteClick={onDeleteClick}
+        onDeleteConfirmClick={onDeleteConfirmClick}
+        onEditClick={onEditClick}
+        onShareNowClick={onShareNowClick}
+      />
+    </div>
   );
 };
 
