bcfb46f6fedfd0062fde8631d7e7448be8b57a75
Emily <eplummer26@gmail.com>
2017-07-21 12:52:06 -0700

    undo changes to queue middleware

--- packages/queue/middleware.js
+++ packages/queue/middleware.js
@@ -1,11 +1,17 @@
+import { actionTypes } from '@bufferapp/profile-sidebar';
+import { actions as dataFetchActions } from '@bufferapp/async-data-fetch';
+
 export default ({ dispatch }) => next => (action) => { // eslint-disable-line no-unused-vars
   next(action);
   switch (action.type) {
-    // case asyncDataActionTypes.FETCH_POSTS_START:
-    //   return {
-    //     ...state,
-    //     loading: true,
-    //   };
+    case actionTypes.SELECT_PROFILE:
+      dispatch(dataFetchActions.fetch({
+        name: 'queuedPosts',
+        args: {
+          profileId: action.profile.id,
+        },
+      }));
+      break;
     default:
       break;
   }
