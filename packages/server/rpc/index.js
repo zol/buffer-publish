be682b3531be70a9ba6bd9f1181be8d69ca7960d
Emily <eplummer26@gmail.com>
2017-07-21 13:14:55 -0700

    remove s lol

--- packages/server/rpc/index.js
+++ packages/server/rpc/index.js
@@ -4,7 +4,7 @@ const loginMethod = require('./login');
 const logoutMethod = require('./logout');
 const profilesMethod = require('./profiles');
 const queuedPostsMethod = require('./queuedPosts');
-const sentPostsMethod = require('./sentPosts');s
+const sentPostsMethod = require('./sentPosts');
 const userMethod = require('./user');
 
 module.exports = checkToken(rpc(
