17d178f192b007c6043431d1979e03e90c4d2f02
Emily <eplummer26@gmail.com>
2017-08-03 13:16:02 -0700

    remove extra excalmation

--- packages/queue/components/QueuedPosts/index.jsx
+++ packages/queue/components/QueuedPosts/index.jsx
@@ -51,7 +51,7 @@ const QueuedPosts = ({
       {total < 1 &&
         <EmptyState
           title={'It looks like you haven\'t got any posts in your queue!'}
-          subtitle={'Click the box above to add a post to your queue!'}
+          subtitle={'Click the box above to add a post to your queue :)'}
           heroImg={'https://s3.amazonaws.com/buffer-publish/images/fresh-queue.png'}
         />
       }
