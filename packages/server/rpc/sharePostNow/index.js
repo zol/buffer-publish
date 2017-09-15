cc00de826ba588c0bbf1fefd2e71e55d2a26e343
Hamish Macpherson <hamstu@gmail.com>
2017-09-15 16:24:17 -0700

    pass through correct error message when share now fails

--- packages/server/rpc/sharePostNow/index.js
+++ packages/server/rpc/sharePostNow/index.js
@@ -4,13 +4,25 @@ const rp = require('request-promise');
 module.exports = method(
   'sharePostNow',
   'share post now',
-  ({ updateId, profileId }, { session }) =>
-    rp({
-      uri: `${process.env.API_ADDR}/1/updates/${updateId}/share.json`,
-      method: 'POST',
-      strictSSL: !(process.env.NODE_ENV === 'development'),
-      qs: {
-        access_token: session.accessToken,
-      },
-    }),
+  async ({ updateId, profileId }, { session }) => {
+    let result;
+    try {
+      result = await rp({
+        uri: `${process.env.API_ADDR}/1/updates/${updateId}/share.json`,
+        method: 'POST',
+        strictSSL: !(process.env.NODE_ENV === 'development'),
+        qs: {
+          access_token: session.accessToken,
+        },
+      });
+    } catch (err) {
+      if (err.error) {
+        const { message } = JSON.parse(err.error);
+        throw new Error(message);
+      }
+      throw err;
+    }
+    result = JSON.parse(result);
+    return Promise.resolve(result);
+  },
 );
